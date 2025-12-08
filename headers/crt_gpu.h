db15531582c6c9ceba6de8511e820933f424abb2
Feir <slavatruhin@gmail.com>
2025-12-08 11:34:19 +1000

    Очистка

--- headers/crt_gpu.h
+++
@@ -1,6 +0,0 @@
-#ifndef CRT_GPU_H
-#define CRT_GPU_H
-
-__global__ void add(unsigned long *a, unsigned long *b, unsigned long *c, unsigned long *prime_set);
-
-#endif
--- sources/crt_gpu.cu
+++
@@ -1,9 +0,0 @@
-#include "crt_gpu.h"
-#include "crt_cpu.h"
-#include <gmpxx.h>
-
-__global__ void add(unsigned long *a, unsigned long *b, 
-    unsigned long *c, unsigned long *prime_set) 
-{
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-}
